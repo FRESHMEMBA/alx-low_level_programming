21ee84005de7505f42e02fdb65016502d714a40f
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-06-22 18:53:17 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,15 +12,14 @@ void print_most_numbers(void)
 
         for (i = 0; i < 10; i++)
         {
-                if (i == 2 || i == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(i + '0');
-		}
+	if (i == 2 || i == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(i + '0');
+	}
         }
-
         _putchar('\n');
 }
