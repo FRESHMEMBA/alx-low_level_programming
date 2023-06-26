30924d9606f6b72730521abe31eb4f9eb8ea6204
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-06-26 14:11:09 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - Takes a pointer to an int as a parameter,
- * teh it updates the value it poits to to 98.
- * n is the poiter pointing to the value to be modified.
+ * then it updates the value it poits to to 98.
+ * @n: The int  poiter pointing to the value to be modified.
  * Return: void
  */
 
