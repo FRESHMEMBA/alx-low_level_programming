accc51114090cf43cc0f370fc7251310622b1a43
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-06-28 06:34:10 -0700

    0. Multiple mains

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Tests a function that printsif an integer o positive or negative.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
