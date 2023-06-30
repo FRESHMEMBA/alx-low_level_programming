0e65d7c9abff19314efb1779e21f5cdcf6bbdaba
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-06-29 18:28:57 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,9 @@
  * _strcmp - Compares two strings.
  * @s1: Pointer to the first string.
  * @s2: Pointer to the second string.
- * Return:
- *   0 if s1 and s2 are equal.
- *   a negative value if s1 is less than s2.
- *   a positive value if s1 is greater than s2.
+ * Return: 0 if s1 and s2 are equal.
+ * a negative value if s1 is less than s2.
+ * a positive value if s1 is greater than s2.
  */
 
 int _strcmp(char *s1, char *s2)
