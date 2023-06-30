e4dfa87f7836364e4b24ac380474a8e4b3d35172
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-06-30 06:48:43 -0700

    4. I am a kind of paranoid in revere. I suspect people of plotting to make me happy

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: n array of integers.
+ * @n: The number of elements in the array.
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	int j = n - 1;
+
+	int temp;
+
+	while (i <= j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j++;
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
 0-strcat.c: Contains a function that concatenates two strings.
+void reverse_array(int *a, int n);
