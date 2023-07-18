d96ffa5e9bf1903e8fb40019316980e875e7c3a7
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-07-17 20:57:40 -0700

    2. A dog will tech you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/2.print_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints a struct dog
+ * @d: Pointer t the struct dog to be printed.
+ * return: void
+ */
+void print_dog(strcu dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->name == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
