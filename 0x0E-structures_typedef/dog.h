ff406ae5866c20af6c41de8fd34a182162cb83f8
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-07-18 17:21:03 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
+/**
+ * struct dog - Defines the attributes of a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
+struct dog {
 	char* name;
 	float age;
 	char* owner;
-} dog_t;
+};
+
+/**
+ * dog_t - Typedef for dog.
+ */
 
 #endif	/*DOG_H*/
