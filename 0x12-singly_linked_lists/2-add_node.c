7f6d3adf68c9820223210bc800dd6f6a7ca7b656
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-07-28 14:08:29 -0700

    2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,9 +8,9 @@
  * @str: Value of th node to be added.
  * Return: Address of the new element, o NULL if it failed.
  */
-list_t add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t new_node;
+	list_t *new_node;
 	unsigned int str_len = 0;
 
 
