7b0d743c978fabfb34e4b61955d1126acfadadab
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-07-29 21:32:18 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 		return (NULL);
 
 	len = strlen(str);
-	str_ptr = (char *)malloc(sizeof(char) * len);
+	str_ptr = (char *)malloc(len);
 	if (str_ptr == NULL)
 		return (NULL);
 
