aa5d4aea02e52087fb7cbf235e705058072be5a4
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-08-01 00:20:26 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,6 +3,27 @@
 #include <string.h>
 
 /**
+ * is_all_spaces - Checks if a string consists of spaces only.
+ * @str: The string to be checked.
+ * Return: True if str consists of spaces only,
+ * and false otherwise.
+ */
+bool is_all_spaces(char *str)
+{
+	int i;
+	bool all_spaces = true;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			all_spaces = false;
+			break;
+		}
+	}
+	return (all_spaces);
+}
+
+/**
  * strtow - Splits a string into words.
  * @str: The string to split.
  * Return: Pointer to an array of strings (words),
@@ -13,10 +34,9 @@ char **strtow(char *str)
 	unsigned int i, j, k, n_chars = 0, n_words = 0;
 	char **words_ptr;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL || *str == '\0' || is_all_spaces(str))
 		return (NULL);
-	if (strlen(str) == 1 && *str == ' ')
-		return (NULL);
+	
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
