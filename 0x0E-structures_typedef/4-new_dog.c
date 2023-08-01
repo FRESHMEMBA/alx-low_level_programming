81988b362b12f91caabcdcaea848a9b9876a2ecb
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-08-01 02:34:28 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,6 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int i, name_len, owner_len;
-	char *d_name, *d_owner;
 	dog_t *d_ptr;
 
 	d_ptr = malloc(sizeof(struct dog));
@@ -21,24 +20,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	name_len = strlen(name);
-	d_name = malloc(sizeof(char) * name_len + 1);
-	if (d_name == NULL)
+	d_name->name = malloc(sizeof(char) * name_len + 1);
+	if (d_name->name == NULL)
+	{
+		free(d_ptr);
 		return (NULL);
+	}
 	for (i = 0; i < name_len; i++)
-		d_name[i] = name[i];
-	d_name[i] = '\0';
+		d_ptr->name[i] = name[i];
+	d_ptr->name[i] = '\0';
 
 	owner_len = strlen(owner);
-	d_owner = malloc(sizeof(char) * owner_len + 1);
-	if (d_owner == NULL)
+	d_ptr->owner = malloc(sizeof(char) * owner_len + 1);
+	if (d_ptr->owner == NULL)
+	{
+		free(d_ptr);
 		return (NULL);
+	}
 	for (i = 0; i < owner_len; i++)
-		d_owner[i] = owner[i];
-	d_owner[i] = '\0';
-
-	d_ptr->name = d_name;
+		d_ptr->owner[i] = owner[i];
+	d_ptr->owner[i] = '\0';
 	d_ptr->age = age;
-	d_ptr->owner = owner;
-
 	return (d_ptr);
 }
