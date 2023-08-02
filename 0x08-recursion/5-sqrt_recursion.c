4c454507d1beefd6b8480933f71508355a37cf98
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-08-02 14:11:16 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,42 @@
 #include "main.h"
+/**
+ * is_perferct_square - Checks if a number is a perfect square.
+ * @num: The number to be checked if it is a perfet square.
+ * @guess: The approximatin of the square root of num.
+ * @low: Lower bound
+ * @high: Upper bound.
+ * Return: The suare root of num if num is a perfect square,
+ * otherwise returns 0.
+ */
+int is_perfect_sqaure(int num int guess, int low, int high)
+{
+	int mid, square;
+
+	if (low > high)
+		return (0);
+	mid = low + (high - low) / 2;
+	square = mid * mid;
+
+	if (square == num)
+		return (mid);
+	else if (square < mid)
+		return (is_perfect_square(num, guess, mid + 1, high));
+	else
+		return (is_perfect_square(num, guess, low, mid - 1));
+}
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number whose square roo  is to be calculated.
- * Return: The squae root o f.
+ * @n: The number whose square root  is to be calculated.
+ * Return: The squae root of if n has a naturl square root,
+ * otherwise returns -1.
  */
-
-int _pow_recursion(int x, double y)
-{
-	if (y < 0)
-		return (-1);
-
-	if (y == 0)
-		return (1);
-
-	return (x * _pow_recursion(x, y - 1));
-}
-
 int _sqrt_recursion(int n)
 {
-	return (_pow_recursion(n, 0.5));
-}	
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (is_perfect_square(n, n / 2, 1, n / 2));
+}
+	
