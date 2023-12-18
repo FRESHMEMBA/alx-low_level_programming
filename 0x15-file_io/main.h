127b5fa28ed45c4c0881637ba155ef01a442cc4a
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-12-18 12:29:32 +0200

    FILE I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-ssize read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
 
