2167dedb5fb9d8710a2abcde8da3227aa7801a89
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-06-05 23:35:28 +0200

    debugging free a dlistint_t

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,11 @@
-#include "lists"
+#include "lists.h"
 
 /**
  * free_dlistint - frees a dlistint_t list.
  * @head: pointer to the head of the list to be freed.
  * Return: None
  */
-void free_dlistint(dlistint *head)
+void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
 
