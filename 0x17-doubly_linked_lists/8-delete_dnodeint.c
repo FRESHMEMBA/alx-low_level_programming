f9be13e25c037172376a6dd0ef93f726a4d1e965
smazibuko023 <smazibuko023@student.wethinkcode.co.za>
2024-06-06 20:41:31 +0200

    added the get_dnodeint_at_index definition in the 8-delete_dnodeint.c source file

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,35 @@
 #include "lists.h"
 
 /**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ * @head: pointer to the first node  of the list.
+ * @index: index of the node to searched for.
+ * Return: the nth node of the list if it exists, NULL otherwise.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	temp = head;
+	i = 0;
+	while (temp)
+	{
+		if (i == index)
+			return (temp);
+
+		i++;
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
+
+
+/**
  * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t
  * linked list.
  * @head: pointer to the list to be modified.
