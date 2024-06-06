f4bb2ef917df239c2b036f5d25104609197cb1b7
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-06-06 20:08:03 +0200

    delete node at index - return -1 if the list is empty, 1 otherwise

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t
+ * linked list.
+ * @head: pointer to the list to be modified.
+ * @index: the index of the node that should be deleted.
+ * Return: 1 if succeeded, -1 if it failed.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+
+	if (!*head)
+		return (-1);
+}
