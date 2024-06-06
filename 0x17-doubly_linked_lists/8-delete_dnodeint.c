e495469cdfd13df74a8419379bfef55b36680c20
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-06-06 20:34:13 +0200

    delete node at index - check different scenarios...

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,9 +9,32 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
+	dlistint_t *temp = get_dnodeint_at_index(*head, index);
 
-	if (!*head)
+	/* if the list is empty or the specified index is out of bound */
+	if (!*head || !temp)
 		return (-1);
+
+	/* if the node at the specified index is the first node */
+	if (temp->prev == NULL)
+	{
+		temp->next->prev = NULL;
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+
+	/* if the node at the specified index is the last node */
+	if (temp->next == NULL)
+	{
+		temp->prev->next = NULL;
+		free(temp);
+		return (1);
+	}
+
+	/*if the node at the specified node is somewhere in between the list*/
+	temp->prev->next = temp->next;
+	temp->next->prev = temp->prev;
+	free(temp);
 	return (1);
 }
