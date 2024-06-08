6aa04caaf8d8189469f1cca14af4ebc7c84416bf
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-06-08 23:30:27 +0200

    generate key for crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -110,11 +110,13 @@ int main(int argc, char **argv)
 		ch = argv[1][vch] * ch;
 		vch = vch + 1;
 	}
+
 	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
 	keygen[3] = ((char *)alph)[f4(argv[1], length)];
 	keygen[4] = ((char *)alph)[f5(argv[1], length)];
 	keygen[5] = ((char *)alph)[f6(argv[1])];
 	keygen[6] = '\0';
+
 	for (ch = 0; keygen[ch]; ch++)
 		printf("%c", keygen[ch]);
 	return (0);
