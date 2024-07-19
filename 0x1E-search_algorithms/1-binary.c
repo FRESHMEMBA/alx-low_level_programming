973aa73ac2c96517c4db790d6cdcb274b168a9e4
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-07-19 22:50:19 +0200

    1-binary Betty compliant

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,16 +9,16 @@
  */
 void print_array(int *array, int first, int last)
 {
-    int i;
+	int i;
 
-    for (i = first; i <= last; i++)
-    {
-        printf("%d", array[i]);
+	for (i = first; i <= last; i++)
+	{
+		printf("%d", array[i]);
 
-        if (i != last)
-            printf(", ");
-    }
-    printf("\n");
+		if (i != last)
+		printf(", ");
+	}
+	printf("\n");
 }
 
 
@@ -33,27 +33,27 @@ void print_array(int *array, int first, int last)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int mid;
-    int first = 0, last = size - 1;
+	int mid;
+	int first = 0, last = size - 1;
 
-    /* return -1 if array is a null pointer or if the array is empty*/
-    if (!array)
-        return (-1);
+	/* return -1 if array is a null pointer or if the array is empty*/
+	if (!array)
+		return (-1);
 
-    while(first <= last)
-    {
-        printf("Searching in array: ");
-        print_array(array, first, last);
-        mid = (first + last) / 2;
+	while (first <= last)
+	{
+		printf("Searching in array: ");
+		print_array(array, first, last);
+		mid = (first + last) / 2;
 
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            first = mid + 1;
-        else
-            last = mid - 1;
-    }
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			first = mid + 1;
+		else
+			last = mid - 1;
+	}
 
-    /* return -1 if the value is not in the array*/
-    return (-1);
+	/* return -1 if the value is not in the array*/
+	return (-1);
 }
