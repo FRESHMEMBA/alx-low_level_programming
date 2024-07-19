5351a38b86afd0a362b6e2e581b72d2f47de6c3f
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-07-19 23:56:10 +0200

    jump search for empty or NULL array

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,21 @@
+#include "search_algos.h";
+
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * using the jump search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located
+ * If value is not present or array is NULL, return -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int step, prev;
+
+	if (!array)
+		return (-1);
+
+	return (-1);
+
+}
